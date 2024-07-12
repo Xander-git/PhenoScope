3f72c63432a8abb39380601e3a17d15a928443a9
Xander <alxxander.nguyen@gmail.com>
2024-07-12 13:01:57 -0700

    added sample img calls

---
+++ sample_data.py
@@ -0,0 +1,10 @@
+from skimage.io import imread
+
+def StdDay1():
+    return imread("./sample_imgs/StandardDay1.jpg")
+
+def StdDay6():
+    return imread("./sample_imgs/StandardDay6.jpg")
+
+def NoiseDay1():
+    return imread("./sample_imgs/NoiseDay1.jpg")
